9cbbbf5d329b31c8918eb68e1f42d137e4069060
Stoner5 <Stoner5@github.com>
2019-08-19 16:31:48 +0600

    Initial commit

---
+++ src/KO.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+
+    var name : String? = "Kotlin let null check"
+    name?.let { println(it) } //prints Kotlin let null check
+    name = null
+
+
+
+}
---
+++ src/MUYUU.java
@@ -0,0 +1,5 @@
+public class MUYUU {
+    public static void main(String[] args) {
+        System.out.println("Hello World !! ");
+    }
+}
